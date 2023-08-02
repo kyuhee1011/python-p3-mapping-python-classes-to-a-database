a635ee4fc6d6087b121a839b7471e5e0927d003c
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-08-02 02:50:50 -0400

    finished

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,38 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+    @classmethod
+    def create_table(cls):
+        sql="""    CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        CURSOR.execute(sql)
+     # ... rest of Song methods
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.album))
+        CONN.commit()
+
+#SQL query to grab the value of the id column of the last inserted row, 
+# and set that equal to the given song instance's id attribute
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
+
+        #create a new Song instance and save it.
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
+    #The return value of create() should always be the object that we created.
